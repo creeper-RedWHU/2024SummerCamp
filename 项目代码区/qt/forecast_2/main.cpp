77820ba710eb7c59a4e92443a1ce790abbdf91a4
yyyrrrr <142312747+yyyrrrr@users.noreply.github.com>
2024-07-04 10:37:02 +0800

    Add files via upload

---
+++ 项目代码区/qt/forecast_2/main.cpp
@@ -0,0 +1,9 @@
+#include <QApplication>
+#include "widget.h"
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    MyWidget widget;
+    widget.show();
+    return app.exec();
+}
