2b3ce7e187840ba2944a64064f601c8347ac2875
yyyrrrr <142312747+yyyrrrr@users.noreply.github.com>
2024-07-04 17:19:15 +0800

    更新的homepage，进行了一定的美化，还需要添加算法（有注释）

---
+++ 项目代码区/qt/homepage_2/forecast.h
@@ -0,0 +1,28 @@
+#ifndef FORECAST_H
+#define FORECAST_H
+
+#include <QWidget>
+#include <QComboBox>
+#include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
+
+class forecast : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit forecast(QWidget *parent = nullptr);
+    ~forecast();
+
+private slots:
+    void onSelectCityClicked();
+    void onPredictClicked();
+
+private:
+    QComboBox *cityBox;
+    QPushButton *btnPredict;
+    QLabel *imagePlaceholder;
+};
+
+#endif // FORECAST_H
---
+++ 项目代码区/qt/homepage_2/todayweather.h
@@ -0,0 +1,50 @@
+#ifndef TODAYWEATHER_H
+#define TODAYWEATHER_H
+
+#include <QWidget>
+#include <QLabel>
+#include <QPixmap>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QPainter>
+#include <QFont>
+#include <QComboBox>  //组合框控件
+
+namespace Ui {
+class todayweather;
+}
+
+class todayweather : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit todayweather(QWidget *parent = nullptr);
+    ~todayweather();
+
+    QPixmap *weatherp;  //天气图标
+    QPainter *painter;
+    void paintEvent(QPaintEvent *event) override; // 声明重写的 paintEvent 函数
+    void showweather(int index);
+
+    QComboBox *cityBox;  //城市的组合框控件
+
+    QLabel *weatherl;  //天气标签
+
+    QLabel *temperaturel;  //气温标签
+    QLabel *windspeedl1;  //风力标签
+    QLabel *windspeedl2;
+    QLabel *winddirection1;  //风向标签
+    QLabel *winddirection2;
+
+    QLabel *city1;  //城市标签
+    QLabel *city2;
+
+    QVBoxLayout *mainLayout;
+    QHBoxLayout *topLayout;
+
+private:
+    Ui::todayweather *ui;
+};
+
+#endif // TODAYWEATHER_H
